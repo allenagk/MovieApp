5b3a85a90fcae6e3bdda5d82385935e969b59be4
Allen Koch <allenagk@gmail.com>
2022-03-20 12:28:44 +0900

    Setup splash screen

--- app/src/main/java/jp/ml/movies/presentation/MainActivity.kt
+++ app/src/main/java/jp/ml/movies/presentation/MainActivity.kt
@@ -3,23 +3,34 @@ package jp.ml.movies.presentation
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.activity.viewModels
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
-import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
 import dagger.hilt.android.AndroidEntryPoint
 import jp.ml.movies.presentation.movie_detail.MovieDetailScreen
 import jp.ml.movies.presentation.movie_list.MovieListScreen
+import jp.ml.movies.presentation.movie_list.MovieListViewModel
 import jp.ml.movies.presentation.ui.theme.MoviesTheme
 
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
+
+    private val viewModel: MovieListViewModel by viewModels()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        installSplashScreen().apply {
+            setKeepOnScreenCondition() {
+                viewModel.state.value.isLoading
+            }
+        }
         setContent {
             MoviesTheme {
                 // A surface container using the 'background' color from the theme
@@ -46,15 +57,10 @@ class MainActivity : ComponentActivity() {
     }
 }
 
-@Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-}
-
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
     MoviesTheme {
-        Greeting("Android")
+
     }
 }
